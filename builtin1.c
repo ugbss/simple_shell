c6d4964f84485eaa618d4bf5c711894d631bddeb
DrSteelfist <106754352+DrSteelfist@users.noreply.github.com>
2022-09-07 23:41:06 +0300

    Update builtin1.c

--- builtin1.c
+++ builtin1.c
@@ -6,7 +6,7 @@
  * @info: Structure containing potential arguments. Used to maintain
  *         constant function prototype.
  * Return: Always 0
- *
+ */
 int _myhistory(info_t *info)
 {
 	print_list(info->history);
